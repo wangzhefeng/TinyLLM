74f534fd42480d64473fbb364b061ad6b9193368
wangzhefeng <wangzhefengr@163.com>
2025-03-02 19:25:41 +0800

    add deepseek-r1-distill-qwen-7B finetuning

--- finetuning/deepseek_lora/data_download.py
+++ finetuning/deepseek_lora/data_download.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 # ***************************************************
-# * File        : prompt_demo.py
+# * File        : data_download.py
 # * Author      : Zhefeng Wang
 # * Email       : wangzhefengr@163.com
-# * Date        : 2025-02-23
-# * Version     : 0.1.022321
+# * Date        : 2025-03-01
+# * Version     : 0.1.030115
 # * Description : description
 # * Link        : link
 # * Requirement : 相关模块版本需求(例如: numpy >= 2.1.0)
@@ -17,10 +17,16 @@ import sys
 ROOT = os.getcwd()
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+
+from modelscope.msdatasets import MsDataset
 
 # global variable
 LOGGING_LABEL = __file__.split('/')[-1][:-3]
 
+
+# data download
+ds = MsDataset.load('AI-ModelScope/medical-o1-reasoning-SFT', trust_remote_code = True)
 
 
 
