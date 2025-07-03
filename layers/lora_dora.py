3f75ed83d7be1909a50d5bb6de50c9a28d66f7e3
wangzhefeng <wangzhefengr@163.com>
2025-07-04 00:38:12 +0800

    update

--- layers/lora_dora.py
+++ layers/lora_dora.py
@@ -43,35 +43,16 @@ class LoRALayer(nn.Module):
         super(LoRALayer, self).__init__()
 
         # A
-        self.A = nn.Parameter(torch.empty(in_dim, rank))
-        nn.init.kaiming_uniform_(self.A, a = math.sqrt(5))
-        # B
-        self.B = nn.Parameter(torch.empty(rank, out_dim))
-        # alpha
-        self.alpha = alpha
-    
-    def forward(self, x):
-        x = self.alpha * (x @ self.A @ self.B)
-
-        return x
-
-
-class LoRALayer_v2(nn.Module):
-    """
-    初始化一个 LoRA Layer，它会创建矩阵 A 和 B，同时设置:  
-        - 秩超参数 rank(r): rank 是一个超参数，用于控制矩阵 A和 B 的内维度。
-          换句话说，该参数决定了 LoRA 引入的额外参数数量，是平衡模型适应性和参数效率的关键因素
-        - 缩放超参数 alpha: alpha 是一个缩放超参数，作用于低秩适配的输出。
-          它主要控制适配层输出对原始层输出的影响程度，可视为调节低秩适配对层输出影响的一种方式
-    """
-    
-    def __init__(self, in_dim, out_dim, rank, alpha):
-        super(LoRALayer_v2, self).__init__()
-
-        # A
+        # version 1
+        # self.A = nn.Parameter(torch.empty(in_dim, rank))
+        # nn.init.kaiming_uniform_(self.A, a = math.sqrt(5))
+        # version 2
         std_dev = 1 / torch.sqrt(torch.tensor(rank).float())
         self.A = nn.Parameter(torch.randn(in_dim, rank) * std_dev)
         # B
+        # version 1
+        # self.B = nn.Parameter(torch.empty(rank, out_dim))
+        # version 2
         self.B = nn.Parameter(torch.zeros(rank, out_dim))
         # alpha
         self.alpha = alpha
@@ -83,12 +64,15 @@ class LoRALayer_v2(nn.Module):
 
 
 class LinearWithLoRA(nn.Module):
+    """
+    与 LinearWithLoRAMerged 等价
+    """
     
     def __init__(self, linear, rank, alpha):
         super(LinearWithLoRA, self).__init__()
 
         self.linear = linear
-        self.lora = LoRALayer_v2(
+        self.lora = LoRALayer(
             linear.in_features, 
             linear.out_features, 
             rank, 
@@ -101,6 +85,34 @@ class LinearWithLoRA(nn.Module):
         return x
 
 
+class LinearWithDoRA(nn.Module):
+    """
+    与 LinearWithDoRAMerged 等价
+    """
+    
+    def __init__(self, linear, rank, alpha):
+        super(LinearWithDoRA, self).__init__()
+        
+        self.linear = linear
+        self.lora = LoRALayer(
+            linear.in_features, 
+            linear.out_features, 
+            rank, 
+            alpha
+        )
+        self.m = nn.Parameter(
+            torch.ones(1, linear.out_features)
+        )
+
+    def forward(self, x):
+        linear_output = self.linear(x)
+        lora_output = self.lora(x)
+        lora_output_norm = lora_output / (lora_output.norm(p=2, dim=1, keepdim=True) + 1e-9)
+        dora_modification = self.m * lora_output_norm
+        
+        return linear_output + dora_modification
+
+
 class LinearWithLoRAMerged(nn.Module):
     """
     This LoRA code is equivalent to LinearWithLoRA
@@ -110,7 +122,7 @@ class LinearWithLoRAMerged(nn.Module):
         super(LinearWithLoRAMerged, self).__init__()
 
         self.linear = linear
-        self.lora = LoRALayer_v2(
+        self.lora = LoRALayer(
             linear.in_features, 
             linear.out_features, 
             rank, 
@@ -118,8 +130,11 @@ class LinearWithLoRAMerged(nn.Module):
         )
 
     def forward(self, x):
+        # combine LoRA matrices
         lora = self.lora.A @ self.lora.B
-        combined_weight = self.linear.weight + lora.T
+        # combine LoRA with original weights
+        combined_weight = self.linear.weight + self.lora.alpha * lora.T
+
         output = F.linear(x, combined_weight, self.linear.bias)
 
         return output
@@ -128,28 +143,37 @@ class LinearWithLoRAMerged(nn.Module):
 class LinearWithDoRAMerged(nn.Module):
     """
     Code inspired by https://github.com/catid/dora/blob/main/dora.py
+    DoRA 可以用两个步骤来描述：
+        - 第一步是将预训练的权重矩阵分解为一个幅度向量（m）和一个方向矩阵（V）
+        - 第二步是对方向矩阵 V 应用 LoRA，并单独训练幅度向量 m。
     """
 
     def __init__(self, linear, rank, alpha) -> None:
         super(LinearWithDoRAMerged, self).__init__()
 
         self.linear = linear
-        self.lora = LoRALayer_v2(
+        self.lora = LoRALayer(
             linear.in_features, 
             linear.out_features, 
             rank, 
             alpha,
         )
+        # magnitude vector(幅度向量, 一个标量值，表示长度)
         self.m = nn.Parameter(
             self.linear.weight.norm(p=2, dim=0, keepdim=True)
         )
 
     def forward(self, x):
+        # combine LoRA matrices
         lora = self.lora.A @ self.lora.B
+        # combine LoRA with original weights
         combined_weight = self.linear.weight + self.lora.alpha * lora.T
+        # directional matrix(方向矩阵, 一个单位向量，表示其在空间中的取向)
         column_norm = combined_weight.norm(p=2, dim=0, keepdim=True)
         V = combined_weight / column_norm
+        # 任何向量都可以表示为其幅度与其方向的乘积,DoRA中，将大小和方向分解应用于整个预训练权重
         new_weight = self.m * V
+        
         output = F.linear(x, new_weight, self.linear.bias)
 
         return output
--- model_post_training/lora_qlora/README.md
+++ model_post_training/lora_qlora/README.md
@@ -4,5 +4,6 @@
 * [Finetuning LLMs with LoRA and QLoRA: Insights from Hundreds of Experiments](https://lightning.ai/pages/community/lora-insights/)
 * [Practical Tips for Finetuning LLMs Using LoRA (Low-Rank Adaptation)](https://magazine.sebastianraschka.com/p/practical-tips-for-finetuning-llms)
 * [Improving LoRA: Implementing Weight-Decomposed Low-Rank Adaptation (DoRA) from Scratch](https://magazine.sebastianraschka.com/p/lora-and-dora-from-scratch)
-* [rasbt/LLM-finetuning-scripts](https://github.com/rasbt/LLM-finetuning-scripts/blob/main/adapter/lora-from-scratch/lora-dora-mlp.ipynb)
+* [rasbt/LLM-finetuning-scripts](https://github.com/rasbt/LLM-finetuning-scripts/tree/main)
 * [rasbt/dora-from-scratch](https://github.com/rasbt/dora-from-scratch)
+* [DoRA](https://github.com/NVlabs/DoRA)
